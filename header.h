84ddff0d2494d75a6ec58c3b7c76c18bfd116cf6
Medusa Galadriel <medusaga@oa-b6.msk.21-school.ru>
2023-12-21 19:58:26 +0300

    with gitignore

--- header.h
+++ header.h
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <strings.h>
-int s21_sprintf(char *str, const char *format, ...);
+
 typedef struct flags{
     int integer;
     int string;
 
 
-}
+} flags;
+
+int s21_sprintf(char *str, const char *format, ...);
+char* write_string(flags Flags, va_list arg);
+char* write_int(flags Flags, va_list arg);
+flags parser(const char *format);
+char*  concat_type(flags Flags, va_list arg);
--- ssprintf_ref.c
+++ ssprintf_ref.c
@@ -1,107 +1,89 @@
 #include "header.h"
 #include <stdarg.h>
+#include <string.h>
 int s21_sprintf(char *str, const char *format, ...){
     
     va_list arg;
     va_start(arg, format);
     char* tmp;
+    flags Flags;
     while(*format!='\0'){
-        if(*format=='%'){
-            parser(format); \\ создаем и зануляем структуру флаги, идем от процента
-           tmp= type(my_struct, va_list);
+        printf("here?");
 
-            s21_strcat(str, tmp);
+        if(*format=='%'){
+           Flags=parser(format); // создаем зануляем и заполняем структуру флаги, идем от процента
+           tmp=concat_type(Flags, arg);
+           strcat(str, tmp);}
+           
             
 
-            if
-        //     printf("");
-       // flag.percent=1;}
-        // }
-        // else{
+          
 *str=*format;
 format++;
 str++;        
+    
     }
     
      //в arg записывается указатель на первый аргумент из ...
 
-int x=va_arg(arg, int); //возвращает значение arg(текущий variable argument)
-int y=va_arg(arg, int); //при последующем вызове вернет следующий аргумент
-char* s=va_arg(arg, char*);
-// printf("x=%d", x);
-// printf("y=%d", y);
-// printf("s=%s", s);
+// int x=va_arg(arg, int); //возвращает значение arg(текущий variable argument)
+// int y=va_arg(arg, int); //при последующем вызове вернет следующий аргумент
+// char* s=va_arg(arg, char*);
+// // printf("x=%d", x);
+// // printf("y=%d", y);
+// // printf("s=%s", s);
    
     va_end(arg);
     
 }
-}
 
-void parser_flags(format){
-    while(*format==+||*format==-){
-        switch(*format){
-            
-        case'+':
-            struct.plus=1;
-        case'-':
-            struct.minus=1;  
-        default:
-            break;  }
-            
-            format++;
+
+
+
+ char*  concat_type(flags Flags, va_list arg){
+    char* add_this;
+    if(Flags.integer==1){
+       add_this=write_int(Flags, arg);
     }
-    
+    else if(Flags.string==1){
+        add_this=write_string(Flags, arg);
+    }
+    return add_this; //мы допишем это в str вместо %d
    }
 
- char*  type(){
-    char* x;
-    if(my_struct.int==1){
-       x= print_int(my_struct, va_list);
-    }
-    else if(my_struct.string==1){
-        x=printf_string()
-    }
-    return x;
-   }
-
-char* printf_int(arg){
+char* write_int(flags Flags, va_list arg){
     char* x=(char*)va_arg(arg, int);
 return x;
 
 }
 
-    flags parser(*format){
-        flags My_flags={0};
-        parser_flags(format, struct);
-        while(((*format=='d')||(*format=='s'))&&(*format!=' ')){
+char* write_string(flags Flags, va_list arg){
+    char* x=va_arg(arg, char*);
+return x;
+
+}
+
+    flags parser(const char *format){
+        flags Flags={0};
+        format++;
+        while(*format!=' '){
+            printf("here?:");
         switch(*format){
             
-        case'+':
-            flags=1;
-        case'-':
-            struct.minus=1;  
+        case'd':
+            Flags.integer=1;
+            break;
+        case's':
+            Flags.string=1;
+            break;  
         default:
             break;  }
             
             format++;
     }
+    return Flags;
 
 
     }
 
-int main(){
-    // char date[] = "21 june 2021";
-    // int day, year;
-    // char month[9];
 
-    // sscanf( date, "%i %s %i", &day, month, &year);
-    // printf("The day is %i , the month is %s, the year is %i ", day, month, year);
-
-    char stdout[100];
-    char s[]="string";
-    s21_sprintf(stdout, "hello%d hello%d %s\n", 1, 2, s);
-    
-    printf("%s", stdout);
-
-
-    }
